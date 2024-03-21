c9deaf4bb5706aa94db389209a594fbbd1eea5b8
Braden Murao <bradenm94@gmail.com>
2024-03-20 23:40:54 -0700

    added student cpt and updated supports for student cpt

--- inc/cpt-taxonomy.php
+++ inc/cpt-taxonomy.php
@@ -75,7 +75,9 @@ $args = array(
     'hierarchical'       => false,
     'menu_position'      => 5,
     'menu_icon'          => 'dashicons-admin-users',
-    'supports'           => array( 'title'),
+    'supports'           => array( 'title','editor', 'thumbnail'),
+    'orderby'            => 'title',
+    'order'              => 'ASC',
     'template' => array(
         
         array( 
@@ -127,7 +129,35 @@ function fwd_register_taxonomies() {
         'query_var'         => true,
         'rewrite'           => array( 'slug' => 'staff-categories' ),
     );
-    register_taxonomy( 'fwd-staff-category', array( 'fwd-staff' ), $args );}
+    register_taxonomy( 'fwd-staff-category', array( 'fwd-staff' ), $args );
+
+    // Add Student Category taxonomy
+    $labels = array(
+        'name'              => _x( 'Student Categories', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Student Category', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Student Categories' ),
+        'all_items'         => __( 'All Student Category' ),
+        'parent_item'       => __( 'Parent Student Category' ),
+        'parent_item_colon' => __( 'Parent Student Category:' ),
+        'edit_item'         => __( 'Edit Student Category' ),
+        'view_item'         => __( 'View Student Category' ),
+        'update_item'       => __( 'Update Student Category' ),
+        'add_new_item'      => __( 'Add New Student Category' ),
+        'new_item_name'     => __( 'New Student Category Name' ),
+        'menu_name'         => __( 'Student Category' ),
+    );
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'show_in_nav_menu'  => true,
+        'show_in_rest'      => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'student-categories' ),
+    );
+    register_taxonomy( 'fwd-student-category', array( 'fwd-students' ), $args );}
 
 add_action( 'init', 'fwd_register_taxonomies');
 
