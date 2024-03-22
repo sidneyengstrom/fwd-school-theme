9deda64c49457823c6555c275f95175205bbe7dc
Sidney Engstrom <sidneyengstrom@gmail.com>
2024-03-21 19:36:05 -0700

    Alters and fixes missing footer content

--- footer.php
+++ footer.php
@@ -12,22 +12,15 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'fwd-school-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'fwd-school-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'fwd-school-theme' ), 'fwd-school-theme', '<a href="https://sidney-engstrom.com/school-site">Sidney Engstrom, Braden Murao</a>' );
-				?>
-		</div><!-- .site-info -->
 		<div class="footer-menu">
 			<?php wp_nav_menu( array( 'theme_location' => 'footer') ); ?>
 		</div>
+		<div class="site-info">
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'fwd-school-theme' ), 'FWD School Theme', '<a href="https://sidney-engstrom.com/school-site">Sidney Engstrom, Braden Murao</a>' );
+				?>
+		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
